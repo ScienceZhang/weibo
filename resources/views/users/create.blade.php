31fcd38908bdab1fd220e0d0eb1ea37008eb9c9a
ScienceZhang <science.k.zhang@gmail.com>
2020-01-02 00:36:16 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,9 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
+
+      @include('shared._errors')
+
       <form method="POST" action="{{ route('users.store') }}">
         {{ csrf_field() }}
 
@@ -27,7 +30,7 @@
         </div>
 
         <div class="form-group">
-          <label for="password_confirmation">密码:</label>
+          <label for="password_confirmation">确认密码:</label>
           <input type="text" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
         </div>
 
